faa0c7f24bf8df91917ac5cfd6ac180da4e0e0d6
Max Hlystov <maxvls@gmail.com>
2016-05-13 13:26:32 +0300

    Added static files

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#!/bin/bash
+ln -sf /home/web/etc/nginx.conf /etc/nginx/nginx.conf
+#/etc/init.d/nginx restart
+#ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#/etc/init.d/gunicorn restart
