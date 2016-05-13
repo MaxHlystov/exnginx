052a0080f3ced94551386e03a6bdf28644406fc3
Max Hlystov <maxvls@gmail.com>
2016-05-13 13:36:26 +0300

    Content directories

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+function change_color() {
+    if (document.getElementById("head").style.color != "blue"){
+		document.getElementById("head").style.color = "blue";
+	}
+	else {
+		document.getElementById("head").style.color = "red";
+	}
+}
