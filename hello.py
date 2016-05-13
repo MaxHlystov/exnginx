0adc02a08c76574e28d729432a18ec451528bed9
Maxim <maxvls@gmail.com>
2016-05-13 22:31:30 +0300

    Make app function needed

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
+from cgi import parse_qs
+
 def app(environ, start_response):
-        data = b"Hello, World!\n"
+        data = ""
+        d = parse_qs(environ['QUERY_STRING'])
+        for q in d:
+            data += '%s=%s\n' % (q, d[q])
         start_response("200 OK", [
             ("Content-Type", "text/plain"),
             ("Content-Length", str(len(data)))
