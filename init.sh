a38eedc705dcd46efcae0d5d22e3eb02a384efeb
Maxim <maxvls@gmail.com>
2016-05-13 21:42:02 +0300

    Added gunicorn

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
-#/etc/init.d/nginx restart
+/etc/init.d/nginx start
 #/etc/rc.d/init.d/nginx
-#ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#/etc/init.d/gunicorn restart
+ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+/etc/init.d/gunicorn start
