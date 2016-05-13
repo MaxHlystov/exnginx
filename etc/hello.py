07886dc0b3899ec23c890793601dbc4adc93a21c
Maxim <maxvls@gmail.com>
2016-05-13 22:56:12 +0300

    To control system

--- etc/hello.py
+++ etc/hello.py
@@ -1,10 +1,17 @@
-bind = "0.0.0.0:8080"
-# или через сокет
-# bind = "unix:/home/proft/projects/blog/run/blog.socket"
-workers = 3
-#user = "box"
-#group = "box"
-logfile = "/home/box/web/etc/gunicorn.log"
-loglevel = "info"
-proc_name = "hello"
-#pidfile = "/home/proft/projects/blog/gunicorn.pid"
+from cgi import parse_qs
+
+def app(environ, start_response):
+        data = ""
+        d = parse_qs(environ['QUERY_STRING'])
+        for q in d:
+            data += q + '='
+            for s in d[q]:
+                    data += s
+            data += '\n'
+            
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return [data]
+        
