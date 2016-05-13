7ec1283b2b5a2b139dfd839cffb235c4f3927b03
Maxim <maxvls@gmail.com>
2016-05-13 23:53:13 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,15 @@
 from cgi import parse_qs
 
 def app(environ, start_response):
-        data = ""
-        d = parse_qs(environ['QUERY_STRING'])
-        for q in d:
-            data += q + '='
-            for s in d[q]:
-                    data += s
-            data += '\n'
+        #data = ""
+        #d = parse_qs(environ['QUERY_STRING'])
+        #for q in d:
+        #    data += q + '='
+        #    for s in d[q]:
+        #            data += s
+        #    data += '\n'
             
-        start_response("200 OK", [
-            ("Content-Type", "text/plain") #,
-            #("Content-Length", str(len(data)))
-        ])
-        return [data]
+        start_response("200 OK", [("Content-Type", "text/plain")])
+        resp = environ['QUERY_STRING'].split("&")
+        resp = [item+"\r\n" for item in resp]
+        return resp
