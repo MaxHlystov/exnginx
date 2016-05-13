8acab35fd844f3f8c5a2d4bd64c1e230efcb6e2c
Maxim <maxvls@gmail.com>
2016-05-13 18:27:52 +0300

    Fix path to nginx.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -sf /home/web/etc/nginx.conf /etc/nginx/nginx.conf
+ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 #/etc/init.d/nginx restart
 #/etc/rc.d/init.d/nginx
 #ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
