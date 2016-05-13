42c96e576f97f5f7e83aae7dd54de6935f224f86
Maxim <maxvls@gmail.com>
2016-05-13 22:58:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 /etc/init.d/nginx start
 #/etc/rc.d/init.d/nginx
-ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-/etc/init.d/gunicorn start
+#ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#/etc/init.d/gunicorn start
+gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app &
+
