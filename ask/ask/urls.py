db481ba6a5b1dd21e6771ede332158db0e9a0190
Maxim <maxvls@gmail.com>
2016-05-19 14:20:22 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,12 +19,12 @@ from qa import views
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    #url(r'^/$', admin.site.urls),
-    #url(r'^signup/', admin.site.urls),
-    #url(r'^login/', admin.site.urls),
+    url(r'^/$', views.test),
+    url(r'^signup/', views.test),
+    url(r'^login/', views.test),
     url(r'^question/(?P<num>[0-9]+)/$', views.test),
-    #url(r'^ask/', admin.site.urls),
-    #url(r'^popular/', admin.site.urls),
-    #url(r'^new/', admin.site.urls),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
 ]
 
