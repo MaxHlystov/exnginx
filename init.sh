30a679409620dd1b0362b9933ce1b43af4752931
Maxim <maxvls@gmail.com>
2016-05-19 15:26:00 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,9 @@ apt-get install nginx
 pip install -U Django
 
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
-/etc/nginx/nginx start
+/etc/init.d/nginx start
+
+cd /home/box/web
 
 gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app &
 
