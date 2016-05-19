d3f314625db2d02296b8a0640b5f6f4bde53a14d
Maxim <maxvls@gmail.com>
2016-05-19 15:22:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+apt-get update
+apt-get install nginx
+pip install -U Django
+
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
-/etc/init.d/nginx start
-#/etc/rc.d/init.d/nginx
-#ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#/etc/init.d/gunicorn start
-#gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app &
+/etc/nginx/nginx start
+
+gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app &
+
+cd ask
+gunicorn -b 0.0.0.0:8000 ask.wsgi &
 
