c7ad0a81ac6ef9222157e51747f687d87f7d37de
Maxim <maxvls@gmail.com>
2016-05-20 17:07:41 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import django # added to fix error "django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet"
+
+
+# added to fix error "django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet"
+django.setup()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
