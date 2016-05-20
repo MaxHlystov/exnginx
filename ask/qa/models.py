198ab1011709653619ba17c744a8538e8e6d3375
Max Hlystov <maxvls@gmail.com>
2016-05-20 16:15:30 +0300

    Add mysql models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QUser(models.Model):
+    """Пользователь системы"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    """Вопрос"""
+
+    title = models.CharField(max_length=200) # заголовок вопроса
+    text = models.TextField() # полный текст вопроса
+    added_at = models.DateTimeField('date added') # дата добавления вопроса
+    rating = models.IntegerField(default=0) # рейтинг вопроса (число)
+    author = models.ForeignKey(QUser, on_delete=models.CASCADE) # автор вопроса
+    # список пользователей, поставивших "лайк"
+    likes = models.ManyToManyField(QUser,related_name="%(app_label)s_%(class)s_related")
+
+
+class Answer(models.Model):
+    """ответ"""
+
+    text = models.TextField() # текст ответа
+    added_at = models.DateTimeField() # дата добавления ответа
+    # вопрос, к которому относится ответ
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(QUser, on_delete=models.CASCADE) # автор ответа
+
