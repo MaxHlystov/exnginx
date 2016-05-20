38a1635585c9d408616a2fc17f48ca48c9d37016
Maxim <maxvls@gmail.com>
2016-05-20 16:21:06 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,9 +6,8 @@ pip install -U Django
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 /etc/init.d/nginx -s start
 
-cd /home/box/web
-
-gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app &
+#cd /home/box/web
+#gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app &
 
 cd ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi &
