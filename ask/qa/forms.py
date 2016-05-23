e0cc251bb2e58e7901f584a33e4d7455cbdfcf06
Max Hlystov <maxvls@gmail.com>
2016-05-24 02:14:12 +0300

    Fix authorization

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -52,7 +52,7 @@ class UserCreateForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password")
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
